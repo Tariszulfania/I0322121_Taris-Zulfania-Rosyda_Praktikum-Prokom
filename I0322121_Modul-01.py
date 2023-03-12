1b9c7e3535a603d9e76c2ac8250a0e2826cf8f82
Tariszulfania <Tariszulfania@student.uns.ac.id>
2023-03-12 12:10:10 +0700

    commit ke 2

---
+++ I0322121_Modul-01.py
@@ -0,0 +1,11 @@
+nama= "Taris Zulfania Rosdya"
+nim= "I0322121"
+kampung_halaman= "Semanggi, Pasar Kliwon, Kota Surakarta "
+visualisasi= "Semanggi berada di paling tenggara Kota Surakarta. kelurahan ini merupakan bagian dari Kecamatan Pasar Kliwon. penduduknya merupakan campuran masyarakat Jawa dan Arab. di Keluraan Semanggi terdapat banyak masjid. Semanggi bagian timur dibatasi oleh Sungai Bengawan Solo. nama Semanggi diperoleh dari tanaman semanggi karena dulu di daerah Semanggi bagian timur banyak ditemukan tanaman semanggi."
+harapan= "harapan saya setelah lulus dari teknik indutri semoga ilmu yang saya dapatkan bisa saya terapkan di dunia kerja, dapat menjadi pengusaha, dan mampu bekerjadenan handal dalam bidangnya"
+
+print(nama)
+print(nim)
+print(kampung_halaman)
+print(visualisasi)
+print(harapan)
